4f1ffc6ac07676c829b33a50b04c9fd996b833b9
Priya-raja <priya.thevar89@gmail.com>
2020-07-30 09:01:35 +0400

    todoapp

---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+//Header for a site
+import React from 'react';
+
+
+function Header() {
+
+    return (
+        
+            <header className = 'header'>Hi Priya!</header>
+
+     
+        );
+}
+
+export default Header;
---
+++ src/components/Tododata.js
@@ -0,0 +1,23 @@
+const Tododata = [
+
+    {
+
+        id: "1",
+        text: "Practice react",
+        completed: true
+
+
+    },
+    {
+        id: "2",
+        text: "Learn Django",
+        completed: false
+
+    },
+    {
+        id: "3",
+        text: "Do SQL",
+        completed: true
+    }
+]
+export default Tododata;
