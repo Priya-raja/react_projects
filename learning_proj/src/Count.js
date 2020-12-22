24e249725c70f588e261b35a6a88d99fa2c632c9
Priya raja <priya.thevar89@gmail.com>
2020-12-22 13:25:47 +0400

    Used useEffect hook

--- learning_proj/src/Count.js
+++ learning_proj/src/Count.js
@@ -1,14 +1,20 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 export default function Count() {
 
     const [count, setCount]  = useState(0);
+    // const [color, setColor]  = useState(null);
+
+    useEffect(() => {
+        document.title = `You clicked ${count} times`});
+    
 
 
     return(
-
+        
         <div>
-            {count}
+
+            <p> u clicked {count}</p>
             <button onClick= { () =>setCount(prevState => prevState - 1)}>-</button>
             <button onClick={()=>setCount(prevState => prevState + 1)}>+</button>
         </div>
--- learning_proj/src/index.js
+++ learning_proj/src/index.js
@@ -2,14 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './style.css';
 // import App from './App';
-// import Count from './Count';
-import Name from './Name';
+import Count from './Count';
+// import Name from './Name';
 
 
 ReactDOM.render(
  
-    // <Count />,
-    <Name />,
+    <Count />,
+    // <Name />,
  
   document.getElementById('root')
 );
