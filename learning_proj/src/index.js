0571936e64d6144b7323869d8beaede9bb1b75a5
Priya raja <priya.thevar89@gmail.com>
2020-12-22 21:39:51 +0400

    fetch api

--- learning_proj/src/index.js
+++ learning_proj/src/index.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import './App.css';
-import App from './App'
+// import App from './App'
+import FetchApp from './Fetchapp'
 
 ReactDOM.render(
-
-  <App />,
+   <FetchApp />,
+  // <App />,
   document.getElementById('root')
 )
