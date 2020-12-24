109707439076195d4ef80c68284174b65026adc7
Priya raja <priya.thevar89@gmail.com>
2020-12-24 20:33:14 +0400

    Added input texts in mem form

--- meme-generator/src/MemeGenerator.js
+++ meme-generator/src/MemeGenerator.js
@@ -3,11 +3,49 @@ import React, {Component} from "react"
 class MemeGenerator extends Component {
     constructor() {
         super()
-        this.state = {}
+        this.state = {
+            topText: "",
+            bottomText: "",
+            randomImg: "http://i.imgflip.com/1bij.jpg",
+            allMemeImgs: []
+        }
     }
+    componentDidMount() {
+
+        fetch('https://api.imgflip.com/get_memes')
+        .then(response => response.json())
+        .then(response => {
+            const {memes} = response.data
+            console.log(memes[0])
+            this.setState({allMemeImgs: memes})
+        })
+    }
+    /**
+     * We'll be using an API that provides a bunch of meme images.
+     * 
+     * Your task:
+     * make an API call to "https://api.imgflip.com/get_memes" and save the 
+     * data that comes back (`response.data.memes`) to a new state property
+     * called `allMemeImgs`. The
+     */
     render() {
         return (
-            <h1>MEME GENERATOR SECTION</h1>
+            <form className="meme-form">
+               <input 
+                        type="text"
+                        name="topText"
+                        placeholder="Top Text"
+                        value={this.state.topText}
+                    /> 
+                    <input 
+                        type="text"
+                        name="bottomText"
+                        placeholder="Bottom Text"
+                        value={this.state.bottText}
+                    /> 
+                <button>Gen</button>
+
+            </form>
         )
     }
         
