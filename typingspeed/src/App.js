42c9f9f5b23f08f7aaed26cc03548e7612fe2383
Priya raja <priya.thevar89@gmail.com>
2020-12-26 08:59:17 +0400

    designed the html for word counting app

---
+++ typingspeed/src/App.js
@@ -0,0 +1,25 @@
+import React from 'react'
+
+function App () {
+  // const [words,setWords] = useState([]);
+  // const [time,setTime]  = useState([0]);
+
+
+    // handleChange = {
+ 
+       
+    // }
+
+  return(
+    <div>
+      <h1>How fast do you type?</h1>
+      <textarea />
+      <h2> Time </h2>
+      <button>Start</button>
+      <h2>Words</h2>
+      
+    </div>
+  )
+}
+
+export default App;
