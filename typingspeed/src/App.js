bcb0e7548bc7025923a68e279e3f95197ea6a6fc
Priya raja <priya.thevar89@gmail.com>
2020-12-27 11:08:15 +0400

    Added useref to change focus of text

--- typingspeed/src/App.js
+++ typingspeed/src/App.js
@@ -1,6 +1,6 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 
-function App () {
+function App() {
   const STARTING_TIME = 5;
   //Set the default value in textarea
   const [text, setText] = useState("")
@@ -9,57 +9,63 @@ function App () {
   const [timeRemaining, setTimeRemaining] = useState(STARTING_TIME)
 
   //settime running
-   const [isTimeRunning, setIsTimeRunning] = useState(false)
+  const [isTimeRunning, setIsTimeRunning] = useState(false)
 
-   const [wordCount, setWordCount] = useState(0)
+  //Count the number of words
+  const [wordCount, setWordCount] = useState(0)
+
+  const textBoxRef = useRef(null)
   function handleChange(e) {
-
-    const {value} = e.target
+    const { value } = e.target
     setText(value)
   }
 
   function calculateWordcount(text) {
-     const wordArr = text.split(' ');
-     
-     return wordArr.filter(word => word!== 0).length
-    }
+    const wordArr = text.split(' ');
+    return wordArr.filter(word => word !== 0).length
+  }
 
   function startGame() {
-      setIsTimeRunning(true)
-      setTimeRemaining(STARTING_TIME)
-      setText("")
+    setIsTimeRunning(true)
+    setTimeRemaining(STARTING_TIME)
+    setText("")
+    textBoxRef.current.disabled = false //as we have set thetextbox disabled
+    textBoxRef.current.focus();
   }
-  
+
   function endGame() {
-      setIsTimeRunning(false)
-      setWordCount(calculateWordcount(text))
+    setIsTimeRunning(false)
+    setWordCount(calculateWordcount(text))
   }
 
   useEffect(() => {
-    if (isTimeRunning && timeRemaining > 0){ 
-    setTimeout(() => {
-         setTimeRemaining(time => time - 1)
-    }, 1000)
-  } else if(timeRemaining === 0) {
-    endGame()
-}
-  },[timeRemaining, isTimeRunning])
+    if (isTimeRunning && timeRemaining > 0) {
+      setTimeout(() => {
+        setTimeRemaining(time => time - 1)
+      }, 1000)
+    } else if (timeRemaining === 0) {
+      endGame()
+    }
+  }, [timeRemaining, isTimeRunning])
 
-  return(
+  return (
     <div>
       <h1>How fast do you type?</h1>
+
       <textarea
-      onChange= {handleChange}
-      value= {text} 
-      disabled={!isTimeRunning}/>
+        ref= {textBoxRef}
+        onChange={handleChange}
+        value={text}
+        disabled={!isTimeRunning} />
+
       <h2> Time: {timeRemaining} </h2>
 
-      <button 
-       onClick={startGame}
-       disabled={isTimeRunning} >
-         Start</button>
+      <button
+        onClick={startGame}
+        disabled={isTimeRunning} >
+        Start</button>
+
       <h2>Words: {wordCount}</h2>
-      
     </div>
   )
 }
