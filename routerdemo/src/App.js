757ca50964ca656e5c2d33baf26b18c98eac1f6c
Priya raja <priya.thevar89@gmail.com>
2020-12-28 21:41:07 +0400

    Added more web pages

--- routerdemo/src/App.js
+++ routerdemo/src/App.js
@@ -1,5 +1,7 @@
 import React from "react"
 import {Link, Route, Switch} from "react-router-dom"
+import Home from './pages/Home'
+import About from './pages/About'
 
 function App () {
   return(
@@ -10,8 +12,8 @@ function App () {
         <Link to="/about"> About </Link>
       </nav>
 <Switch>
-  <Route exact path="/" render ={ () => <h1>This is Home</h1>}/> 
-  <Route path="/about" render = {()=> <h1>This is about</h1>}/> 
+  <Route exact path="/"><Home /></Route> 
+  <Route path="/about"><About/> </Route> 
 
 </Switch>
 
---
+++ routerdemo/src/pages/Profile/Info.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Info () {
+    return (
+        <div>
+            <h1> This is a Profile Info page </h1>
+        </div>
+
+    )
+}
+
+export default Info;
