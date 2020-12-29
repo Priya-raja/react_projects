1a2442471211709a4c1369b9183313a8646e55dc
Priya raja <priya.thevar89@gmail.com>
2020-12-29 09:27:01 +0400

    fetched the service with matching id

--- routerdemo/src/services/ServiceDetail.js
+++ routerdemo/src/services/ServiceDetail.js
@@ -1,12 +1,16 @@
 import React from "react"
  import {useParams} from "react-router-dom"
+ import serviceData from "./servicesData"
 
 
 function ServiceDetail(props) {
-    const params = useParams()
-    console.log(params)
+    const {serviceId} = useParams()
+    const thisService = serviceData.find(service => service._id === serviceId)
     return (
+        <div>
         <h1>Service Detail Page</h1>
+        <p>{thisService.name} - ${thisService.price}</p>
+        </div>
     )
   
 }
