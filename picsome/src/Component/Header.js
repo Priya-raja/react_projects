32eab2b835e14c6709039a8ede91d7ac6d6b6149
Priya raja <priya.thevar89@gmail.com>
2020-12-31 21:15:14 +0400

    Created initial setup for picture site

---
+++ picsome/src/Component/Header.js
@@ -0,0 +1,15 @@
+import React from "react"
+import "../App.css"
+
+function Header() {
+    return(
+       <header>
+           <h2>Picsome Pics</h2>
+           <i className="ri-shopping-cart-line ri-fw ri-2x"></i>
+       </header>
+      
+        
+    )
+}
+
+export default Header;
