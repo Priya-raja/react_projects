b45b5097745e56c6f0718566e5d75ad793a4a63c
Priya raja <priya.thevar89@gmail.com>
2021-01-04 18:14:44 +0400

    add number of items in cart page

--- picsome/src/pages/Cart.js
+++ picsome/src/pages/Cart.js
@@ -1,13 +1,19 @@
-import React from "react"
-import "../App.css"
+import React, {useContext} from "react"
+import {Context} from "../Context"
+import CartItem from "../Component/CartItem"
 
 function Cart() {
-    return(
-      <main className="cart-page">
-          <h1>Cart Page</h1>
-
-      </main>  
+    const {cartItems} = useContext(Context)
+    const cartItemElements = cartItems.map(item => (
+        <CartItem key={item.id} item={item} />
+    ))
+    
+    return (
+        <main className="cart-page">
+            <h1>Check out</h1>
+            {cartItemElements}
+        </main>
     )
 }
 
-export default Cart;
+export default Cart
