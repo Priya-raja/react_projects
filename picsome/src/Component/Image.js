63931dab7087bc322448b538f434bc799089ab40
Priya raja <priya.thevar89@gmail.com>
2021-01-04 17:24:40 +0400

    Added a filled heart icon for favourites

--- picsome/src/Component/Image.js
+++ picsome/src/Component/Image.js
@@ -4,8 +4,16 @@ import {Context} from "../Context"
 function Image({className, img}) {
     const [hovered, setHovered] = useState()
     const {toggleFavorite} = useContext(Context)
-    console.log(hovered)
-    const heartIcon = hovered && <i className="ri-heart-line favorite" onClick={() => toggleFavorite(img.id)}></i>
+   
+    function heartIcon() {
+        if(img.isFavorite) {
+            return <i className="ri-heart-fill favorite" onClick={() => toggleFavorite(img.id)}></i>
+        } else if(hovered) {
+            return <i className="ri-heart-line favorite" onClick={() => toggleFavorite(img.id)}></i>
+        }
+    }
+    
+
     const cartIcon = hovered && <i className="ri-add-circle-line cart"></i>
 
     return (
@@ -15,7 +23,7 @@ function Image({className, img}) {
              onMouseLeave ={() => setHovered(false)}
             >
             <img src={img.url} className="image-grid" alt="random iamges"/>
-            {heartIcon}
+            {heartIcon()}
             {cartIcon}
 
         </div>
--- picsome/src/Context.js
+++ picsome/src/Context.js
@@ -15,10 +15,23 @@ function ContextProvider({children}) {
 
     },[])
 
-    console.log(allPhotos)
+    function toggleFavorite(id) {
+        const updatedArr = allPhotos.map(photo => {
+            if(photo.id === id) {
+                console.log(id)
+                console.log(!photo.isFavorite)
+                return {...photo, isFavorite: !photo.isFavorite}
+            }
+            return photo
+        })
+        
+        setAllPhotos(updatedArr)
+    }
+    
+
 
 return (
-        <Context.Provider value= {{allPhotos}}>
+        <Context.Provider value= {{allPhotos, toggleFavorite}}>
             {children}
         </Context.Provider>
     )
