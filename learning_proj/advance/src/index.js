102a8ebc6f59e6e53369e787f1168d9242a9276d
Priya raja <priya.thevar89@gmail.com>
2021-01-06 09:46:22 +0400

    Mood tracker

--- learning_proj/advance/src/index.js
+++ learning_proj/advance/src/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import Count from './Count'
+import Count from './Count';
+import Mood from './Mood';
 import './App.css';
 
 ReactDOM.render(
   // <App />,
-   <Count />,
+  //  <Count />,
+   <Mood />,
   document.getElementById('root')
 )
    
