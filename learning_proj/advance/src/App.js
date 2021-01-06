5ab2d4e671ce82705f3bddcfaa5f6780901986b8
Priya raja <priya.thevar89@gmail.com>
2021-01-06 07:42:59 +0400

    Used props and children in badge

--- learning_proj/advance/src/App.js
+++ learning_proj/advance/src/App.js
@@ -1,3 +1,26 @@
+import React from 'react'
 
 
+const Badge = ({children, color, ...props}) => {
+    return <span className = {`badge ${color}`} {...props}>{children}</span>
+}
+    
+function App () {
+
+    return (
+        <section>
+
+         <h1>Check out these badges</h1>
+
+      <Badge color="green" onClick ={() =>{
+          console.log("I am clicked")
+      }}>Success</Badge> This is operational. <br />
+      <Badge color="red">Removed</Badge> This is critical. <br />
+      <Badge color="yellow">Warning</Badge> This is a warning. <br />
+      <Badge color="blue">Beta</Badge> This is in progress. <br />
+
+        </section>
+    )
+}
+export default App
 
--- learning_proj/advance/src/index.js
+++ learning_proj/advance/src/index.js
@@ -1,25 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
-// const reactElement = <div className="fish">Heellllooooo</div>
-
-const Button = ({ children, onClick }) => {
-    return <button onClick={onClick}> {children}</button>
-}
-
-const domElement = document.getElementById('root')
+import App from './App';
+import './App.css';
 
 ReactDOM.render(
-    <div>
-      <Button onClick = {() => {
-    console.log("I was clicked")
-}}>
-        + Add
-      </Button>
-      <Button>
-        Subtract -
-      </Button>
-      <Button children="MULTIPLY *****" />
-    </div>, domElement)
-    
+  <App />,
+  document.getElementById('root')
+)
    
