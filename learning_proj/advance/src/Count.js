3eb05b928cfa6fb8cda0bef23bfd501042fd6bda
Priya raja <priya.thevar89@gmail.com>
2021-01-06 08:36:07 +0400

    change color on state change

--- learning_proj/advance/src/Count.js
+++ learning_proj/advance/src/Count.js
@@ -3,7 +3,8 @@ import React, {useState} from 'react'
 function Count () {
 
     const [count, setCount] = useState(0)
-    
+    const [someColor, setSomeColor] = useState(10)
+
     const add = () => {
 
         setCount(count + 1)
@@ -22,12 +23,18 @@ function Count () {
        
        <section>
         <h1> Count the number of times clicked</h1>
+        <div style= {{color: `hsl(${someColor}, 100%, 50%)`}}> Hi there!</div>
         <div>
             <button onClick = {subtract}>-</button>
 
-            <input type= 'text'
+            <input type= 'number'
             aria-label= 'count'
-            value = {count} />
+            // controlled component
+            value = {count} 
+            onChange= {(event) => {
+               setCount(parseInt(event.target.value)) //if not parsed then it just adds a string of 1
+                setSomeColor(count  * 10)
+            }}/> 
             <button onClick = {add}>+</button>        
         </div>
         </section>
