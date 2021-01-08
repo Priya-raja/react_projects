7310bf1ef8b3d30509afd6fc47c981e01b87dbd1
Priya raja <priya.thevar89@gmail.com>
2021-01-08 11:33:04 +0400

    Added all the design elements and draggable

--- designdnd/src/App.js
+++ designdnd/src/App.js
@@ -3,26 +3,31 @@ import "./App.css"
 
   const Design = ({children, size, ...props}) => {
     return (
-      <div className = {`${size}`} {...props}>{children}</div>  
+      <div>
+      <div className = {`${size}`} {...props} draggable="true">{children}</div>  
+      </div>
     )
   }
 function App() {
   return(
     <main>
-      <div className="container"></div>
+      <div className="palettes">
+      
       <div className="container">
+        <h3>Drawing Palette</h3>
         
-       <Design size = "image">Image</Design>
-       <Design size = "text">Text</Design>
-       <Design size = "text">Price</Design>
-       <Design size = "text">Source code</Design>
-       <Design size = "text">Expiration date</Design>
-       
+      </div>
+      <div className="container">
+      <h3>Design Objects Palette</h3>
+       <Design  size = "image" >Image</Design>
+       <Design  size = "text">Text</Design>
+       <Design  size = "text">Price</Design>
+       <Design  size = "text">Source code</Design>
+       <Design  size = "text">Expiration date</Design>
        </div>
-    
+       </div>
+       <div className="name"></div>    
     </main>
-    
-
   )
 }
 export default App;
