045439d4983c1e0275b761a1197fd9809f9a5099
Priya raja <priya.thevar89@gmail.com>
2021-01-08 08:08:11 +0400

    Made the design items

--- designdnd/src/App.js
+++ designdnd/src/App.js
@@ -1,11 +1,28 @@
 import React from "react"
 import "./App.css"
 
+  const Design = ({children, size, ...props}) => {
+    return (
+      <div className = {`${size}`} {...props}>{children}</div>  
+    )
+  }
 function App() {
-  return (
-    <div className="container">
-      <textarea/>  
-    </div>
+  return(
+    <main>
+      <div className="container"></div>
+      <div className="container">
+        
+       <Design size = "image">Image</Design>
+       <Design size = "text">Text</Design>
+       <Design size = "text">Price</Design>
+       <Design size = "text">Source code</Design>
+       <Design size = "text">Expiration date</Design>
+       
+       </div>
+    
+    </main>
+    
+
   )
 }
 export default App;
