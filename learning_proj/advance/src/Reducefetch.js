58c2c1dc4da9e718f56b64a78bfe29866c9c1d09
Priya raja <priya.thevar89@gmail.com>
2021-01-21 21:39:53 +0400

    USed loading and error

--- learning_proj/advance/src/Reducefetch.js
+++ learning_proj/advance/src/Reducefetch.js
@@ -10,23 +10,64 @@ fetch('https://jsonplaceholder.typicode.com/todos/1')
 */
 
 const Reducefetch = () => {
-  let [user, setUser] = useState(null);
+ let [user, setUser] = useState(null);
+  let [state, dispatch] = useReducer((state, action) => {
+    switch(action.type) {
+      case 'LOADING': {
+        return { ...state, loading: true }
+      }
+      case 'RESOLVED': {
+        return {
+          ...state,
+          loading: false,
+          response: action.response,
+          error: null
+        }
+      }
+      case 'ERROR': {
+        return {
+          ...state,
+          loading: false,
+          response: null,
+          error: action.error
+        }
+      }
+      default:
+        return state
+    }
+  }, {
+    loading: false,
+    response: null,
+    error: null
+  })
+  
   
   useEffect(() => {
+    let isCurrent = true
+    dispatch({ type: "LOADING" })
     fetch('https://jsonplaceholder.typicode.com/todos/1')
       .then(response => response.json())
       .then(json => {
-        setUser(json)
+        if (isCurrent) {
+          dispatch({ type: "RESOLVED", response: json })
+        }
+      }).catch(error => {
+        dispatch({ type: "ERROR", error })
       })
+    return () => {
+      isCurrent = false
+    }
   }, [])
+  
+  let { response } = state
   
   return (
     <section>
       <h2>Get User Data</h2>
       <div className="user">
-        {user && <>
-          User ID: {user.id} <br />
-          User Title: {user.title}
+        {response && <>
+          User ID: {response.id} <br />
+          User Title: {response.title}
         </>}
       </div>
     </section>
